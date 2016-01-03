d08c34ce3a284db7039d37c23f3fe72df1057995
toshi0383 <t.suzuki326@gmail.com>
2016-01-03 22:45:50 +0900

    update bootstrap.sh: Improved standard out

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
-echo -n "Initializing your dotfiles in your OS `uname -s`. Are you sure ? (y/n):"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+NORMAL="\033[0;39m"
+echo -en "${YELLOW}Initializing your dotfiles in your OS `uname -s`. Are you sure ? (y/n):${NORMAL}"
+
 read line
+
 if [ $line != y ];then
     echo "Exiting..."
     exit 1;
 fi
-cp $PWD/.bashrc ~/.bashrc
-cp $PWD/.vimrc ~/.vimrc
-cp $PWD/.gitconfig ~/.gitconfig
+
+copy() {
+    echo -n "Copying ${1} ... "
+    cp $PWD/${1} ~/${1}
+    echo "done"
+}
+
+copy .bashrc
+copy .vimrc
+copy .gitconfig
 
 if [ "`uname -s`" == "Linux" ];then
-    cp $PWD/.Xmodmap ~/.Xmodmap
+    copy .Xmodmap
     which setxkbmap > /dev/null
     if [ $? -ne 0 ];then
+        echo "Installing setxkbmap as root"
         sudo apt-get install setxkbmap
     fi
 fi
 
-GREEN="\033[32m"
-NORMAL="\033[0;39m"
 echo -e "${GREEN}Finished bootstrapping. Please restart your Terminal.${NORMAL}"
 
