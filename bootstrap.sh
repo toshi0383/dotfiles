379befb0548066b58780840395ad98ae23599fc3
toshi0383 <t.suzuki326@gmail.com>
2016-01-04 10:24:24 +0900

    install lv and tree in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,18 @@ copy() {
     echo "done"
 }
 
+install_additional_commands() {
+    for i in tree lv
+    do
+        which $i > /dev/null
+        if [ $? -ne 0 ];then
+            echo -n "Installing ${i} ... "
+            sudo apt-get --quiet install $i
+            echo "done"
+        fi
+    done
+}
+
 copy .bashrc
 copy .vimrc
 copy .gitconfig
@@ -24,10 +36,12 @@ copy .gitconfig
 if [ "`uname -s`" == "Linux" ];then
     which setxkbmap > /dev/null
     if [ $? -ne 0 ];then
-        sudo apt-get install setxkbmap
+        sudo apt-get --quiet install setxkbmap
     fi
     copy .Xmodmap
 fi
+
+install_additional_commands
 
 echo -e "${GREEN}Finished bootstrapping. Please restart your Terminal.${NORMAL}"
 
